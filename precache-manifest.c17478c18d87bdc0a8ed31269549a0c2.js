e7d93b24cb93d853a3cf283db520cd29759f81b9
Ansukun5852 <gouda.ansukun5852.com>
2021-02-23 12:06:08 +0530

    Updates

---
+++ precache-manifest.c17478c18d87bdc0a8ed31269549a0c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da6b5ab56fb03f38f087f4ab26d727c0",
+    "url": "/Mesigo/index.html"
+  },
+  {
+    "revision": "60a1391b03e084f803a4",
+    "url": "/Mesigo/static/css/main.c9527f51.chunk.css"
+  },
+  {
+    "revision": "ce95205ca66be11f1dcd",
+    "url": "/Mesigo/static/js/2.76387e4b.chunk.js"
+  },
+  {
+    "revision": "5ac922b1a6e253946f13fa455dc8d57c",
+    "url": "/Mesigo/static/js/2.76387e4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60a1391b03e084f803a4",
+    "url": "/Mesigo/static/js/main.383edef6.chunk.js"
+  },
+  {
+    "revision": "b98e579e120b2a9bb0c8",
+    "url": "/Mesigo/static/js/runtime-main.6b14b7d2.js"
+  },
+  {
+    "revision": "6933297f6dd015f7f4701fe1bd5b0e98",
+    "url": "/Mesigo/static/media/image.6933297f.jpg"
+  }
+]);
